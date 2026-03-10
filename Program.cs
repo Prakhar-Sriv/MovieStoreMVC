29f806e9686eabb02a6f548c5df552c6d12846a8
Prakhar Srivatava <sprakhar2004@gmail.com>
2026-03-10 18:03:31 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,48 +1,3 @@
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-//using MovieStoreMvc.Models.Domain;
-//using MovieStoreMvc.Repositories.Abstract;
-//using MovieStoreMvc.Repositories.Implementation;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
-
-//builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
-
-//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-//    .AddEntityFrameworkStores<DatabaseContext>()
-//    .AddDefaultTokenProviders();
-//builder.Services.AddScoped<IMovieService, MovieService>();
-
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-//app.UseAuthentication();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller= UserAuthentication}/{action=Register}");
-
-//app.Run();
-
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieStoreMvc.Models.Domain;
@@ -91,3 +46,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
